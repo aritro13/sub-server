d7f81abd7470813f0bc93c44e06a549209cd206d
aritro13 <aritro13@gmail.com>
2021-04-21 17:00:28 +0000

    crash on connection loss fixed

--- subServer.py
+++ subServer.py
@@ -9,44 +9,39 @@ logger = logging.getLogger(__name__)
 class server:
 
 	def __init__(self):
-		
-		while True:
-			if(!is_socket_closed()):
-				self.operations()
-			else:
-				try:
-					self.connectToMainServer()
-				except:
-					print "Main Server Down"
-	
-	def connectToMainServer():
-		self.name = "SUB-SERVER"
-		self.sock = socket.socket()
-		self.host = socket.gethostname()
-		self.port = 5000
-		self.sock.bind(("192.168.1.102", 0))
-		self.sock.connect(("192.168.1.101", self.port))
+                self.connectToMainServer()
+                self.startServer()
 
-	def is_socket_closed() -> bool:
-		try:
-			# this will try to read bytes without blocking and also without removing them from buffer (peek only)
-			data = self.sock.recv(16, socket.MSG_DONTWAIT | socket.MSG_PEEK)
-			if len(data) == 0:
-				return True
-		except BlockingIOError:
-			return False  # socket is open and reading from it would block
-		except ConnectionResetError:
-			return True  # socket was closed for some other reason
-		except Exception as e:
-			logger.exception("unexpected exception when checking if a socket is closed")
-			return False
-		return False
+        def startServer(self):
+		while True:
+                            try:
+                                print "Starting Ops"
+                                self.operations()
+                            except Exception as e:
+                                print "Operations Failed: ", e
+                                self.connectToMainServer()
+
+	def connectToMainServer(self):
+	    self.name = "SUB-SERVER"
+            self.sock = socket.socket()
+            self.port = 5000
+            self.host = socket.gethostname()
+            self.sock.bind(("192.168.1.102", 0))
+            try:
+		self.sock.connect(("192.168.1.101", self.port))
+                print "Connection Established"
+            except Exception as e:
+                print "Main Server Down", e
+                sleep(1)
 
 	def operations(self):
 		msg = self.sock.recv(1024)
+                print "MSG:", msg, type(msg)
 		if msg == 'WHO':
-				self.sock.send('123123123*'+self.name)
-		if '*' in msg:
+		    self.sock.send('123123123*'+self.name)
+                if msg == '':
+                    self.connectToMainServer()
+                if '*' in msg:
 			msg = msg.split('*')
 			print "Query : ",msg
 			if msg[0] == 'PREPARE':
@@ -64,7 +59,7 @@ class server:
 					os.remove(msg[1])
 					print "Deleted",msg[1]
 				else:
-					print msg[1],"File is not exists!!!!!!!!!"
+					print msg[1],"File does not exists!!!!!!!!!"
 
 
 server1 = server()
